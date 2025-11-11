8cd3d008fc5197765e7c0e7e9fd46667b8eb670d
Mason <esslinger.mason@proton.me>
2025-11-11 02:36:26 -0700

    make the command do something

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import sys
+
+from lib.core import CoreIter
+from lib.regex import Regex
+
+
 if __name__ == "__main__":
-    raise NotImplementedError("Main is not implemented yet")
+    # first 2 args will be main.py and {path}/rexer
+    if len(sys.argv) < 3:
+        raise Exception("No arguments to parse")
+
+    CoreIter(sys.argv[2:]).foreach(lambda s: print(Regex(s)))
