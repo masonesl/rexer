c916d17ae34d70bcb49ea904a95547d6b0c0f19e
Mason <esslinger.mason@proton.me>
2025-11-11 02:22:42 -0700

    add character classes

---
+++ lib/char.py
@@ -0,0 +1,99 @@
+from typing import Self, assert_never, final, override
+
+from lib.core import Consts, CoreIter
+
+
+@final
+class CharClass:
+
+    @override
+    def __str__(self) -> str:
+        ss: list[str] = []
+        if len(chars := self.group_chars()) > 1 or isinstance(chars[0], tuple):
+            ss.append(Consts.OBRACK)
+
+        for ch in chars:
+            match ch:
+                case str(ch):
+                    ss.append(ch)
+                case tuple((start_ch, end_ch)):
+                    ss.append(f"{start_ch}{Consts.DASH}{end_ch}")
+                case _:
+                    assert_never(ch)
+
+        if ss[0] == Consts.OBRACK:
+            ss.append(Consts.CBRACK)
+
+        return ''.join(ss)
+
+    @override
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __init__(self):
+        self.__chars: list[str] = []
+
+    def add_char(self, ch: str):
+        if len(ch) == 0:
+            raise Exception("Cannot add empty string to character class")
+        if len(ch) > 1:
+            raise Exception("Cannot add multi-character string to character class")
+
+        self.__chars.append(ch)
+        self.__chars.sort()
+
+    def add_char_range(self, start_ch: str, end_ch: str):
+        if not (ord(start_ch) < ord(end_ch)):
+            raise Exception("Cannot add character range where the start character is not less than the end character")
+
+        CoreIter(range(ord(start_ch), ord(end_ch) + 1)).foreach(
+            lambda ch_n: self.add_char(chr(ch_n))
+        )
+
+    def add_alpha(self):
+        self.add_char_range('A', 'Z')
+        self.add_char('_')
+        self.add_char_range('a', 'z')
+
+    def add_numeric(self):
+        self.add_char_range('0', '9')
+
+    def add_whitespace(self):
+        self.add_char(' ')
+
+    def with_whitespace(self) -> Self:
+        self.add_whitespace()
+        return self
+
+    def with_numeric(self) -> Self:
+        self.add_numeric()
+        return self
+
+    def with_alpha(self) -> Self:
+        self.add_alpha()
+        return self
+
+    def with_char_range(self, start_ch: str, end_ch: str) -> Self:
+        self.add_char_range(start_ch, end_ch)
+        return self
+
+    def with_char(self, ch: str) -> Self:
+        self.add_char(ch)
+        return self
+
+    def group_chars(self) -> list[str | tuple[str, str]]:
+        chars: list[str | tuple[str, str]] = []
+
+        cit = CoreIter(self.__chars)
+        ch = cit.next()
+        while ch is not None:
+            start_ch = end_ch = ch
+            while (ch := cit.next()) is not None and ord(ch) == ord(end_ch) + 1:
+                end_ch = ch
+
+            if start_ch == end_ch:
+                chars.append(end_ch)
+            else:
+                chars.append((start_ch, end_ch))
+
+        return chars
